a886517067cebfd69a4c263e340e926546cefd52
Ankit-kumar8-1 <ankitksoftware@gmail.com>
2026-04-05 11:40:33 +0530

    feat: initial project setup with Spring Boot 4.0 & Java 21
    - Integrated Spring Security with JWT & RBAC support
    - Configured Spring Data JPA with MySQL Connector
    - Added Lombok and Validation dependencies
    - Basic project structure and Maven configuration completed

---
+++ spring-security-jwt-rbac/src/main/java/in/ankit/spring_security_jwt_rbac/SpringSecurityJwtRbacApplication.java
@@ -0,0 +1,14 @@
+package in.ankit.spring_security_jwt_rbac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityJwtRbacApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityJwtRbacApplication.class, args);
+		System.out.println("Application started successfully!");
+	}
+
+}
---
+++ spring-security-jwt-rbac/src/test/java/in/ankit/spring_security_jwt_rbac/SpringSecurityJwtRbacApplicationTests.java
@@ -0,0 +1,13 @@
+package in.ankit.spring_security_jwt_rbac;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityJwtRbacApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
